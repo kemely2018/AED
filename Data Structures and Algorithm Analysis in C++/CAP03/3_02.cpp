fe38243b540d27dfb66c43ffd2d9a19645ae0dd5
kemely2018 <41025582+kemely2018@users.noreply.github.com>
2019-09-17 09:57:56 -0500

    Add files via upload

---
+++ Data Structures and Algorithm Analysis in C++/CAP03/3_02.cpp
@@ -0,0 +1,22 @@
+/*2.	Cambie dos elementos adyacentes ajustando solo los enlaces 
+(y no los datos) usando:
+a. singly linked lists */
+
+#include<iostream>
+#include "lista_enlazada.h"
+using namespace std;
+
+int main(){
+	Lista_enlazada<int> l_int;
+    l_int.insertar(2);
+    l_int.insertar(1);
+    l_int.insertar(3);
+    l_int.insertar(8);
+    l_int.insertar(5);
+    l_int.mostrar();
+    l_int.swap_adyacente(2);
+    l_int.mostrar();
+    l_int.swap_adyacente(4);
+    l_int.mostrar();
+}
+
